b0fd9ac1f72bf75f26447ce07a57cb97928291d2
刘俊(壹钱包) <LIUJUN223@pingan.com.cn>
2020-10-04 08:08:27 +0800

    add sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+source ~/.bash_profile
+export LANG=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+git submodule init
+git submodule update
+#重置每个子项目的工作区，拉取最新代码
+cd mou1
+git reset --hard
+git pull origin main
+git checkout main
+
+cd mou3
+git submodule init
+git submodule update
+
+cd mou2
+git reset --hard
+git pull origin main
+git checkout main
+
+
